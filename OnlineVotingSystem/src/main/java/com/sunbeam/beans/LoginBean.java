47716f53e82c2cc9d2f8b9e0ea068fa2094631ff
Tejas Junghare <tejasjunghare09@gmail.com>
2024-12-07 00:31:21 +0530

    minor error fixes occured during voting

--- OnlineVotingSystem/src/main/java/com/sunbeam/beans/LoginBean.java
+++ OnlineVotingSystem/src/main/java/com/sunbeam/beans/LoginBean.java
@@ -5,85 +5,47 @@ import com.sunbeam.daos.UserDaoImpl;
 import com.sunbeam.entities.User;
 
 public class LoginBean {
-	
 	private String email;
 	private String password;
 	private User user;
 	
-	
 	public LoginBean() {
-		
 	}
-	
-
-
-
-	public LoginBean(String email, String password, User user) {
-		super();
-		this.email = email;
-		this.password = password;
-		this.user = user;
-	}
-
-
-
 
 	public String getEmail() {
 		return email;
 	}
 
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 
 	public String getPassword() {
 		return password;
 	}
 
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-
-	
-
 
 	public User getUser() {
 		return user;
 	}
 
-
-
-
 	public void setUser(User user) {
 		this.user = user;
 	}
-
-
-
-
+	
 	public void login() {
 		try(UserDao userDao = new UserDaoImpl()) {
 			User dbUser = userDao.findByEmail(this.email);
-			if(dbUser!=null && dbUser.getPassword().equals(this.password)) {
-				this.user=dbUser;
-			}
-			
-			else {
-				this.user= null;
-				
-			}
-			
+			if(dbUser != null && dbUser.getPassword().equals(this.password))
+				this.user = dbUser;
+			else
+				this.user = null;
 		} catch (Exception e) {
 			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
-		
-
 	}
-	
-	
-
 }
--- OnlineVotingSystem/src/main/java/com/sunbeam/daos/UserDao.java
+++ OnlineVotingSystem/src/main/java/com/sunbeam/daos/UserDao.java
@@ -15,4 +15,5 @@ public interface UserDao extends AutoCloseable {
 	User findById(int id) throws Exception;
 	User findByEmail(String email) throws Exception;
 	int save(User u) throws Exception;
+	int update(User u) throws Exception;
 }
